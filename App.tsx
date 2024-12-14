6a8d23f550ad175b3c8431fbff0083dd030c686a
sapna sharma <sapnasharma000786@gmail.com>
2024-12-14 22:58:24 +0530

    first commit

---
+++ App.tsx
@@ -0,0 +1,17 @@
+import {Button, Text,View } from 'react-native';
+import React from 'react';
+
+const App = () => {
+  return (
+    <View>
+ <Text style = { { fontSize : 30 } }>React Native Mobile</Text>
+ <Text style = { { fontSize : 30 } }>React Native Mobile</Text>
+ <Text style = { { fontSize : 30 } }>React Native Mobile</Text>
+ <Button title='Click Me'/>
+    </View>
+     
+    
+  );
+};
+
+export default App;
